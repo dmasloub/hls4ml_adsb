a8fb30017cf8f14f4f16ee28056019adb3f5166e
dmasloub <david.masloub@googlemail.com>
2024-04-08 13:50:47 +0200

    add create_dirs.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hls4ml_adsb
+
+## system req
+- ubuntu 20.04 
+- vivado v2020.1
---
+++ create_dirs.py
@@ -0,0 +1,11 @@
+from constants import *
+import os
+
+if not os.path.exists(MODEL_STANDARD_DIR):
+    os.makedirs(MODEL_STANDARD_DIR)
+
+if not os.path.exists(MODEL_LSTM_DIR):
+    os.makedirs(MODEL_LSTM_DIR)
+
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
